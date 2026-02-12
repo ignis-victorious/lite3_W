190997f14edc8a0353579c0d71c885fc3b7a083f
Emmanuele Levi <ignisvictorious@gmail.com>
2026-02-12 16:29:31 +0000

    Create 2 functions to insert data into tables

--- src/connect.py
+++ src/connect.py
@@ -30,25 +30,79 @@ sql_statements: list[str] = [
 ]
 
 
-# create a database connection
-try:
-    with sqlite3.connect(database=database) as conn:
-        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
-        # interact with database
-        cursor: Cursor = conn.cursor()
+def add_project(conn, project) -> int | None:
+    # insert table statement
+    sql: str = """ INSERT INTO projects(name,begin_date,end_date)
+              VALUES(?,?,?) """
 
-        # execute all statements to create tables (projects & tasks)
-        for statement in sql_statements:
-            cursor.execute(statement)  # cursor.execute(first_table)
-        print("Tables created successfully.")
+    # Create  a cursor
+    cur: Cursor = conn.cursor()
 
-        # commit the changes
-        conn.commit()
-        # pass
+    # execute the INSERT statement
+    cur.execute(sql, project)
 
-except sqlite3.OperationalError as e:
-    print(f"Database error: {e}", e)
-    # print("Failed to open database:", e)
+    # commit the changes
+    conn.commit()
+
+    # get the id of the last inserted row
+    return cur.lastrowid
 
 
-# if __name__ == "__main__":
+def add_task(conn, task) -> int | None:
+    # insert table statement
+    sql: str = """INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
+             VALUES(?,?,?,?,?,?) """
+
+    # create a cursor
+    cur: Cursor = conn.cursor()
+
+    # execute the INSERT statement
+    cur.execute(sql, task)
+
+    # commit the changes
+    conn.commit()
+
+    # get the id of the last inserted row
+    return cur.lastrowid
+
+
+def main():
+
+    # create a database connection
+    try:
+        with sqlite3.connect(database=database) as conn:
+            print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+            # interact with database
+            cursor: Cursor = conn.cursor()
+
+            # execute all statements to create tables (projects & tasks)
+            for statement in sql_statements:
+                cursor.execute(statement)  # cursor.execute(first_table)
+            print("Tables created successfully.")
+
+            # commit the changes
+            conn.commit()
+            # pass
+
+            # add  a project
+            project_one: tuple[str, str, str] = ("Cool App with SQLite & Python", "2015-01-01", "2015-01-30")
+            project_id: int | None = add_project(conn=conn, project=project_one)
+            print(f"Created a project with the id {project_id}")
+
+            # add tasks to the project
+            tasks: list[tuple[str, int, int, int | None, str, str]] = [
+                ("Analyze the requirements of the app", 1, 1, project_id, "2015-01-01", "2015-01-02"),
+                ("Confirm with user about the top requirements", 1, 1, project_id, "2015-01-03", "2015-01-05"),
+            ]
+
+            for task in tasks:
+                task_id: int | None = add_task(conn=conn, task=task)
+                print(f"Created task with the id {task_id}")
+
+    except sqlite3.OperationalError as e:
+        print(f"Database error: {e}", e)
+        # print("Failed to open database:", e)
+
+
+if __name__ == "__main__":
+    main()
