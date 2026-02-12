56808c533dcde0a5d83e0d48e4ff9e758e392bbc
Emmanuele Levi <ignisvictorious@gmail.com>
2026-02-12 15:39:53 +0000

    Create open connection

---
+++ src/connect.py
@@ -0,0 +1,19 @@
+#
+#   Import LIBRARIES
+import sqlite3
+
+#   Import FILES
+#  ______________________
+#
+
+
+try:
+    with sqlite3.connect(database="src/data/my.db") as conn:
+        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+        # interact with database
+        pass
+except sqlite3.OperationalError as e:
+    print("Failed to open database:", e)
+
+
+# if __name__ == "__main__":
